fd8c2699723157ffac173f83ae684cb4ce372314
xwp <xwp_fullstack@163.com>
2019-02-28 12:23:42 +0800

    改善注册体验

--- apps/user/views.py
+++ apps/user/views.py
@@ -22,93 +22,6 @@ import time
 
 
 # /user/register
-# GET POST PUT DELETE OPTION
-# def register(request):
-#     '''注册'''
-#     if request.method == 'GET':
-#         # 显示注册页面
-#         return render(request, 'register.html')
-#     else:
-#         # 进行注册处理
-#         # 接收数据
-#         username = request.POST.get('user_name')
-#         password = request.POST.get('pwd')
-#         email = request.POST.get('email')
-#         allow = request.POST.get('allow')
-
-#         # 进行数据校验
-#         if not all([username, password, email]):
-#             # 数据不完整
-#             return render(request, 'register.html', {'errmsg': '数据不完整'})
-
-#         # 校验邮箱
-#         if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
-#             return render(request, 'register.html', {'errmsg': '邮箱格式不正确'})
-
-#         if allow != 'on':
-#             return render(request, 'register.html', {'errmsg': '请同意协议'})
-
-#         # 校验用户名是否重复
-#         try:
-#             user = User.objects.get(username=username)
-#         except User.DoesNotExist:
-#             # 用户名不存在
-#             user = None
-
-#         if user:
-#             # 用户名已存在
-#             return render(request, 'register.html', {'errmsg': '用户名已存在'})
-
-#         # 进行业务处理: 进行用户注册
-#         user = User.objects.create_user(username, email, password)
-#         user.is_active = 0
-#         user.save()
-
-#         # 返回应答, 跳转到首页
-#         return redirect(reverse('goods:index'))
-
-
-# def register_handle(request):
-#     '''进行注册处理'''
-#     # 接收数据
-#     username = request.POST.get('user_name')
-#     password = request.POST.get('pwd')
-#     email = request.POST.get('email')
-#     allow = request.POST.get('allow')
-
-#     # 进行数据校验
-#     if not all([username, password, email]):
-#         # 数据不完整
-#         return render(request, 'register.html', {'errmsg':'数据不完整'})
-
-#     # 校验邮箱
-#     if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
-#         return render(request, 'register.html', {'errmsg':'邮箱格式不正确'})
-
-#     if allow != 'on':
-#         return render(request, 'register.html', {'errmsg':'请同意协议'})
-
-#     # 校验用户名是否重复
-#     try:
-#         user = User.objects.get(username=username)
-#     except User.DoesNotExist:
-#         # 用户名不存在
-#         user = None
-
-#     if user:
-#         # 用户名已存在
-#         return render(request, 'register.html', {'errmsg':'用户名已存在'})
-
-#     # 进行业务处理: 进行用户注册
-#     user = User.objects.create_user(username, email, password)
-#     user.is_active = 0
-#     user.save()
-
-#     # 返回应答, 跳转到首页
-#     return redirect(reverse('goods:index'))
-
-
-# /user/register
 class RegisterView(View):
     '''注册'''
     def get(self, request):
@@ -164,8 +77,7 @@ class RegisterView(View):
         send_register_active_email.delay(email, username, token)
 
         # 返回应答, 跳转到首页
-        return redirect(reverse('goods:index'))
-
+        return render(request, 'register.html', {'errmsg': '注册成功'})
 
 class ActiveView(View):
     '''用户激活'''
